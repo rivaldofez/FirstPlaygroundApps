e6a4445e0c3e6488e351ffd3fe43d7cc60cbd02e
rivaldofez <rivaldofez@gmail.com>
2022-04-14 00:59:46 +0700

    play music with subsonic

--- ContentView.swift
+++ ContentView.swift
@@ -1,10 +1,25 @@
 import SwiftUI
+import Subsonic
 
 struct ContentView: View {
+    let names = ["Sophie", "Charlotte", "Andrian"]
+    
+    
     var body: some View {
         NavigationView {
-            List {
-                Text("Hello World!")
+            ScrollView {
+                ForEach(names, id: \.self) { name in
+                    
+                    Button {
+                        play(sound: "Sophie.mp3")
+                    } label: {
+                    Image(name)
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(25)
+                        .padding(.horizontal)
+                    }
+                }
             }
             .navigationTitle("Friend Face")
         }
